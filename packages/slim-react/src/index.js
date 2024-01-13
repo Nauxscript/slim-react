f28b3cd9208705eeecaa9f3aeb43012b76c37488
nauxscript <nauxscript@gmail.com>
2024-01-13 19:45:17 +0800

    refactor: setup export

--- packages/slim-react/src/index.js
+++ packages/slim-react/src/index.js
@@ -1,2 +1,10 @@
-export * from "@slim-react/react-dom"
-export * from '@slim-react/core'
+// export * from "@slim-react/react-dom"
+// export * from '@slim-react/core'
+
+import { createRoot } from '@slim-react/react-dom';
+import { createElement } from '@slim-react/core';
+
+export default {
+  createRoot,
+  createElement
+}
