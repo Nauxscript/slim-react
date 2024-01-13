e836bf5b2b09a3ba19b6ad92c94d475ea33b5079
nauxscript <nauxscript@gmail.com>
2024-01-13 12:37:23 +0800

    chore: setup basic example

---
+++ packages/slim-react/example/day-1-basic-render/main.js
@@ -0,0 +1,18 @@
+// ------------ target -------------
+// import { createRoot } from 'react-dom/client';
+
+// function App() {
+//   return <h1>app</h1>;
+// }
+
+// const app = document.getElementById('#app');
+// const root = createRoot(app);
+// root.render(<App />);
+
+
+import { createRoot, init } from '../dist/slim-react.esm-bundler.js';
+
+createRoot()
+init()
+
+
