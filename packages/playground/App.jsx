30d9b6c93727f163fe0ce2852cdcd0cddbf6b506
nauxscript <nauxscript@gmail.com>
2024-01-17 11:24:32 +0800

    chore: update playground

--- packages/playground/App.jsx
+++ packages/playground/App.jsx
@@ -1,10 +1,23 @@
-function Counter({num}) {
+let num = 10
+let titleProps = {
+  id: 'counter-title',
+  style: 'border: 1px solid gray',
+  foo: 1
+}
 
+function Counter({title}) {
   const handleClick = () => {
     console.log('click');
+    titleProps = {
+      id: 'counter-title-2',
+      role: 'title'
+    }
+    num++
+    React.update()
   }
     
   return <>
+    <h2 {...titleProps}>{title}</h2>
     <button onClick={handleClick}> +1 s </button>
     <div> Counter: {num}s </div>
   </> 
@@ -14,11 +27,12 @@ function App() {
   return (
     <div id='app'>
       <div>
-        <span>Hello</span>
-        <span>,</span>
+        <h1>
+          <span>Hello, </span>
+          <span>Slim React!</span>
+        </h1>
       </div>
-      <div>Slim React!</div>
-      <Counter num={10} ></Counter>
+      <Counter title="This is a freaking counter" ></Counter>
     </div>
   )  
 }
